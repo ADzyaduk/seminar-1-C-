7e9a1d60104bb8a5bd3c25cd49d4462a38785ec4
Alex Dz <avdzyaduk@gmail.com>
2022-09-30 22:15:15 +0300

    1

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,39 +1,45 @@
-﻿Console.Clear();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
 
-Console.Write("Введите число");
+        Console.Write("Введите число");
 
-int x = int.Parse(Console.ReadLine()!);
+        int x = int.Parse(Console.ReadLine()!);
 
-if (x == 1) 
-{
-    Console.WriteLine("Понедельник"); 
-}
-if (x == 2) 
-{
-    Console.WriteLine("Вторник");
-}
-if (x == 3)
-{
-    Console.WriteLine("Среда");
-}
-if (x == 4)
-{
-    Console.WriteLine("Четверг");
-}
-if (x == 5)
+        if (x == 1)
+        {
+            Console.WriteLine("Понедельник");
+        }
+        if (x == 2)
+        {
+            Console.WriteLine("Вторник");
+        }
+        if (x == 3)
+        {
+            Console.WriteLine("Среда");
+        }
+        if (x == 4)
+        {
+            Console.WriteLine("Четверг");
+        }
+        if (x == 5)
 
-{
-     Console.WriteLine("Пятница");
-}
-if (x == 6)
-{
-     Console.WriteLine("Суббота");
-}
-if (x == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-else
-{
-    Console.WriteLine("Не сегодня!");
+        {
+            Console.WriteLine("Пятница");
+        }
+        if (x == 6)
+        {
+            Console.WriteLine("Суббота");
+        }
+        if (x == 7)
+        {
+            Console.WriteLine("Воскресенье");
+        }
+        else
+        {
+            Console.WriteLine("Не сегодня!");
+        }
+    }
 }
