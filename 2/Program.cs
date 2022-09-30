296ceb0696c8e138047a81e0e3c48d2771b3be88
Alex Dz <avdzyaduk@gmail.com>
2022-09-30 21:30:18 +0300

    Задача 1

---
+++ 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+Console.Write("Введите первое число");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число");
+int z = int.Parse(Console.ReadLine()!);
+
+    if (x * x == z) 
+{
+    Console.WriteLine("yes");
+}
+    else
+{
+    Console.WriteLine("no");
+}
