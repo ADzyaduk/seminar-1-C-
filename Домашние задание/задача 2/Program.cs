6e1c43c3bc871fcfac2d6a086794a8fb4d769004
AlexanderD <avdzyaduk@gmail.com>
2022-10-01 21:41:24 +0300

    Домашние задание задача 2

---
+++ Домашние задание/задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2
+Console.Clear();
+Console.Write("Введите первое число");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число");
+int y = int.Parse(Console.ReadLine()!);
+if (x<y)
+{
+    Console.WriteLine($"{y}");
+}
+if (x>y)
+{
+    Console.WriteLine($"{x}");
+}
