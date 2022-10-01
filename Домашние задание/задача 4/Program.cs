2d196bc7c4f988bbf83c36de2654fe700654f736
AlexanderD <avdzyaduk@gmail.com>
2022-10-01 23:45:26 +0300

    Задача 4

---
+++ Домашние задание/задача 4/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 4
+        // Задача 2
+        Console.Clear();
+        Console.Write("Введите первое число");
+        int x = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите второе число");
+        int y = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите третье число");
+        int z = int.Parse(Console.ReadLine()!);
+        int m = x;
+        if (x > y)
+        {
+            m = x;
+        }
+        if (z > m)
+        {
+            m = z;
+            Console.WriteLine(m);
+        }
+    }
+}
