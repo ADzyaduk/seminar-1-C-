af65153436d0fd5e549284e1a3af721a905cbb05
AlexanderD <avdzyaduk@gmail.com>
2022-10-01 21:31:01 +0300

    Задача 5

---
+++ 5/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите трехзначное число");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{x%10}");
