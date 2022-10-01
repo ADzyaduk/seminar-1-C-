7d6d7f8c4f27cb2da080d9e89303bc63a05069cc
AlexanderD <avdzyaduk@gmail.com>
2022-10-01 18:15:29 +0300

    Исправил задачу 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -12,28 +12,28 @@
         {
             Console.WriteLine("Понедельник");
         }
-        if (x == 2)
+        else if (x == 2)
         {
             Console.WriteLine("Вторник");
         }
-        if (x == 3)
+        else if (x == 3)
         {
             Console.WriteLine("Среда");
         }
-        if (x == 4)
+        else if (x == 4)
         {
             Console.WriteLine("Четверг");
         }
-        if (x == 5)
+        else if (x == 5)
 
         {
             Console.WriteLine("Пятница");
         }
-        if (x == 6)
+        else if (x == 6)
         {
             Console.WriteLine("Суббота");
         }
-        if (x == 7)
+        else if (x == 7)
         {
             Console.WriteLine("Воскресенье");
         }
