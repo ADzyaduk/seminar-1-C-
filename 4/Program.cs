6586ec4a752c24569214acbaae93354cac8285c9
AlexanderD <avdzyaduk@gmail.com>
2022-10-01 21:17:25 +0300

    Задача 4

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите число");
+int x = int.Parse(Console.ReadLine()!);
+int y = -x;
+while(y <= x)
+{
+Console.Write(y);
+y = y+1; 
+}
+
