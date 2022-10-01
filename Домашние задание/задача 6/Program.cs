54538bab888ad3ee4e77540ef8db358df0cf1ac9
AlexanderD <avdzyaduk@gmail.com>
2022-10-01 23:57:07 +0300

    Задача 6

---
+++ Домашние задание/задача 6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите число: ");
+        int x = int.Parse(Console.ReadLine()!);
+        if (x % 2 == 0)
+            Console.WriteLine("Четное");
+        else
+            Console.WriteLine("Нечетное");
+        
+    }
+}
