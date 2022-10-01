21ee329d314cc38615ba849ae972092f0d1cfb69
AlexanderD <avdzyaduk@gmail.com>
2022-10-02 00:25:21 +0300

    Задача 8

---
+++ Домашние задание/задача 8/Program.cs
@@ -0,0 +1,11 @@
+﻿// See Задача 8
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+int y = 0;
+while (y <= x)
+{
+   if (y % 2 == 0)
+   Console.WriteLine($"{y}");
+   y = y+1; 
+}
